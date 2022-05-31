d0688bacc3a5770a1c0904b999c64960db46b56c
phambao7320 <hoangbaongoc7320@gmail.com>
2022-05-31 15:04:17 +0700

    change1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Banner from './components/Banner';
+import Section1 from './components/Section1';
+import Section2 from './components/Section2';
+import Section3 from './components/Section3';
+import Section4 from './components/Section4';
+import Section5 from './components/Section5';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='AbsCenter'>
+        <Header/>
+        <Banner/>
+        <Section1/>
+        <Section2/>
+        <Section3/>
+        <Section4/>
+        <Section5/>
+      </div>
+      <div className='FooterAbs'>
+        <div className='AbsCenter'>
+          <Footer/>
+        </div>
+      </div>
     </div>
   );
 }
